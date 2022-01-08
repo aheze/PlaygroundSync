28a09d7e6d3c0c4ec12e888c0c2ddf875e44e92f
Zheng <aheze@getfind.app>
2022-01-08 08:11:42 -0800

    Initial Commit

---
+++ PlaygroundSync/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  PlaygroundSync
+//
+//  Created by A. Zheng (github.com/aheze) on 1/8/22.
+//  Copyright © 2022 A. Zheng. All rights reserved.
+//
+    
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ PlaygroundSync/PlaygroundSyncApp.swift
@@ -0,0 +1,19 @@
+//
+//  PlaygroundSyncApp.swift
+//  PlaygroundSync
+//
+//  Created by A. Zheng (github.com/aheze) on 1/8/22.
+//  Copyright © 2022 A. Zheng. All rights reserved.
+//
+    
+
+import SwiftUI
+
+@main
+struct PlaygroundSyncApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
